73390483e84405cfa317d29adc367e463cad806c
metalmiketh <mike.c.sutton87@gmail.com>
2018-01-12 15:25:13 +0000

    Changed Player Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,13 +8,11 @@ public class CameraController : MonoBehaviour {
 
     private Vector3 offset;
 
-	// Use this for initialization
 	void Start ()
     {
         offset = transform.position - player.transform.position;
 	}
 	
-	// Update is called once per frame
 	void LateUpdate ()
     {
         transform.position = player.transform.position + offset;
